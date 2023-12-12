3b18f36f5e14968acfd5558a92879f237cadae09
sonpal parmar <109973419+sonpalparmar@users.noreply.github.com>
2023-12-12 19:25:14 +0530

    Create 04_solution.sql

---
+++ 04_solution.sql
@@ -0,0 +1,5 @@
+SELECT v.customer_id, COUNT(v.visit_id) AS count_no_trans
+FROM visits AS v
+LEFT JOIN transactions AS t ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id;
